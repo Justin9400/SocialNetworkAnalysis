3968ec93baec9c9695d1c1515031f2adf0644927
Justin Kachornvanich <justkachorn@gmail.com>
2024-06-17 00:39:59 -0400

    Working on extracting source emails

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import pandas as pd 
+
+df = pd.read_csv('enron.csv')
+
+def clean_data():
+    removed_duplicates_data = df.drop_duplicates()
+    removed_nan_data = removed_duplicates_data.dropna()
+    # removed_nan_data.to_csv('temp.csv')
+    return removed_nan_data
+
+def extract_source_emails(data):
+    source_emails = pd.DataFrame()
+    for i in range(len(data.index)):
+        if "RE:" in data['subject'].iloc[i]:
+            source_emails.
 def main(): 
-    return
+    cleaned_data = clean_data()
+    extract_source_emails(cleaned_data)
+    # print(cleaned_data)
+    # for i in range(len(cleaned_data.index)):
+    #     print(df['subject'][i])
+
+
+
+main()
