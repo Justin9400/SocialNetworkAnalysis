4d167e3d2993c5ecf9fa845e1a389d16fd2ddc6e
Justin9400 <57471411+Justin9400@users.noreply.github.com>
2024-06-28 13:55:30 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import pandas as pd 
+import numpy as np
 
 df = pd.read_csv('enron.csv')
 
-def clean_data():
-    removed_duplicates_data = df.drop_duplicates()
-    removed_nan_data = removed_duplicates_data.dropna()
-    # removed_nan_data.to_csv('temp.csv')
-    return removed_nan_data
+def clean_data(path):
+    df = pd.read_csv(path)
+    df['subject'].replace(to_replace=['', '#NAME?', 'RE:'], value=np.nan, inplace=True)
+
+    df.dropna(subset=['subject'], inplace=True)
+    df = df[~df.duplicated(subset=['subject'])]
+    return df
 
 def extract_source_emails(data):
     source_emails = pd.DataFrame()
-    for i in range(len(data.index)):
-        if "RE:" in data['subject'].iloc[i]:
-            source_emails.
+    count = df['subject'].str.contains('Re:', case=False).sum()
+    print(count)
+
+    
 def main(): 
-    cleaned_data = clean_data()
+    cleaned_data = clean_data('enron.csv')
     extract_source_emails(cleaned_data)
-    # print(cleaned_data)
-    # for i in range(len(cleaned_data.index)):
-    #     print(df['subject'][i])
-
-
 
 main()
